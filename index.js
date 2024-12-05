39ed483d6953a80fb0751bbfb1b82a5b8e89b5df
Yash Kumar <128486714+Hero-Alpha@users.noreply.github.com>
2024-12-05 15:44:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 // CONNECT TO MONGODB
-connectDB();
+
 
 // MIDDLEWARE TO PARSE JSON REQUESTS
 app.use(express.static(path.join(__dirname, "public")));
