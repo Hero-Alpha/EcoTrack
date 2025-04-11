341c5a55f6020aa2cf27136d27ea603e472ee99d
Yash Kumar <128486714+Hero-Alpha@users.noreply.github.com>
2025-04-11 13:56:17 +0530

    Update company.js

--- controllers/company.js
+++ controllers/company.js
@@ -23,7 +23,7 @@ exports.createCompany = async (req, res) => {
     const newCompany = new Company({ name, address, email, contact });
     await newCompany.save();
     console.log("✅ Company saved!");
-    res.redirect('/userHome');
+    res.redirect('/userhome');
   } catch (error) {
     console.error("❌ Failed to save company:", error);
     res.status(400).json({ error: error.message });
